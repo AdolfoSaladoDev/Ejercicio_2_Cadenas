be217d1f38a9dce2c8f7b3facaa8909c7698022c
AdolfoSaladoDev <adolfo.salado.lopez@avanade.com>
2023-04-11 11:04:25 +0200

    Ordenar métodos según su aparición en el programa.

--- Ejercicio2/Program.cs
+++ Ejercicio2/Program.cs
@@ -12,6 +12,64 @@
         }
 
         /// <summary>
+        /// Método encargado de pedir al usuario la cadena que se desea utilizar durante el programa.
+        /// </summary>
+        /// <returns></returns>
+        private static string GetUserString()
+        {
+            bool isCorrectString = false;
+            string userString = "";
+
+            while (!isCorrectString)
+            {
+                Console.WriteLine("Introduzca una cadena de, al menos, 40 caracteres: ");
+                userString = Console.ReadLine();
+
+
+                if (userString.Length >= 40)
+                {
+                    Console.WriteLine("Cadena válida.");
+
+                    isCorrectString = true;
+                }
+                else
+                {
+                    Console.WriteLine($"\nLa cadena introducida posee únicamente {userString.Length} caracteres." +
+                        $" Por favor, introduzca una cadena con, al menos, 40 caracteres.");
+                }
+            }
+
+            return userString;
+        }
+
+
+        /// <summary>
+        /// Método encargado de mostrar la cadena que ha introducido el usuario.
+        /// </summary>
+        private static void ShowString()
+        {
+            Console.Clear();
+            Console.WriteLine("La cadena es la siguiente: ");
+            Console.WriteLine(userString);
+        }
+
+        /// <summary>
+        /// Método encargado de pintar por consola la cadena introducida por el usuario y las opciones que puede realizar.
+        /// </summary>
+        /// <param name="userString"></param>
+        private static void PaintOptionsMenu(string userString)
+        {
+            ShowString();
+
+            Console.WriteLine("\nOpciones:");
+            Console.WriteLine("\t1.- Sustituir palabra.");
+            Console.WriteLine("\t2.- Buscar texto.");
+            Console.WriteLine("\t3.- Buscar texto de inicio.");
+            Console.WriteLine("\t4.- Conversión de dígito.");
+            Console.WriteLine("\t5.- Salir.");
+        }
+
+        /// <summary>
         /// Método encargado de realizar la lógica de las diferentes opciones del menú. 
         /// </summary>
         private static void OptionMenu()
@@ -49,130 +107,13 @@
         }
 
         /// <summary>
-        /// Método encargado de añadir ceros a la izquierda hasta completar una longitud de 12. 
+        /// Método encargado de mostrar la despedida al usuario cuando finaliza el programa.
         /// </summary>
-        private static void ConvertDigitsOption()
+        private static void ExitOption()
         {
-            int numberUser;
-
-            bool isNumber = false;
-
-            while (!isNumber)
-            {
-                Console.Clear();
-                Console.WriteLine("Ha elegido la opción Conversión de dígito.");
-                Console.Write("Introduzca un dígito: ");
-                string numberUserString = Console.ReadLine();
-
-                // Comprobando si se ha introducido un número.
-                isNumber = int.TryParse(numberUserString, out numberUser);
-
-                if (isNumber)
-                {
-                    if (numberUserString.Length < 12)
-                    {
-                        int differenceNumber = 12 - numberUserString.Length;
-
-                        // Añade el carácter deseado a la izquierda, en este caso, 0. 
-                        string newStringWithZero = numberUserString.PadLeft(differenceNumber, '0');
-
-                        Console.WriteLine("\nLa cadena resultante es: " + newStringWithZero);
-                        Console.ReadKey();
-
-                    }
-                    else
-                    {
-                        Console.WriteLine("\nLa cadena tiene un tamaño mayor o igual a 12");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("No ha introducido dígitos correctamente.");
-                }
-            }
-        }
-
-        /// <summary>
-        /// Método encargado de comprobar si una cadena empieza por una cadena introducida por el usuario.
-        /// </summary>
-        private static void SearchSringStart()
-        {
-            bool isCorrect = false;
-
-            while (!isCorrect)
-            {
-                Console.Clear();
-                ShowString();
-                Console.WriteLine("Ha elegido la opción Buscar texto de inicio:");
-
-                Console.Write("Introduzca el texto deseado: ");
-                string textToSearch = Console.ReadLine();
-
-                if (!string.IsNullOrEmpty(textToSearch))
-                {
-                    if (userString.StartsWith(textToSearch))
-                    {
-                        Console.WriteLine($"\nLa cadena comienza por \'{textToSearch}\'.");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"\nLa cadena no comienza por \'{textToSearch}\'.");
-                    }
-
-                    Console.ReadKey();
-                    isCorrect = true;
-                }
-                else
-                {
-                    Console.WriteLine("\nDebe añadir un texto. Introduzca una cadena correcta.");
-                    Console.ReadKey();
-                }
-            }
-        }
-
-        /// <summary>
-        /// Método encargado de buscar si un texto introducido por el usuario se encuentra dentro de la cadena.
-        /// </summary>
-        private static void SearchTextOption()
-        {
-
-            bool isNotNull = false;
-
-            while (!isNotNull)
-            {
-                Console.Clear();
-                ShowString();
-
-                Console.WriteLine("\nHa elegido la opción Buscar texto: ");
-
-                ShowString();
-
-                Console.Write("\n¿Qué texto desea buscar?: ");
-                string textToSearch = Console.ReadLine();
-
-                if (!string.IsNullOrEmpty(textToSearch))
-                {
-                    if (userString.Contains(textToSearch))
-                    {
-                        Console.Clear();
-                        Console.WriteLine($"\nLa cadena \'{textToSearch}\' se encuentra dentro del texto.");
-                        Console.ReadKey();
-                        isNotNull = true;
-                    }
-                    else
-                    {
-                        Console.Clear();
-                        Console.WriteLine("La cadena introducida no se encuentra en el texto");
-                        Console.ReadKey();
-                    }
-                }
-                else
-                {
-                    Console.Clear();
-                    Console.WriteLine("No puede estar vacía. Introduzca una cadena correcta.");
-                    Console.ReadKey();
-                }
-            }
+            Console.Clear();
+            Console.WriteLine("Gracias por usar este software. ¡Hasta la próxima!");
+            Console.ReadKey();
         }
 
         /// <summary>
@@ -237,28 +178,8 @@
             }
         }
 
-        /// <summary>
-        /// Método que realiza la lógica de sustitución de una palabra por otra dentro de la cadena.
-        /// </summary>
-        /// <param name="userString">Cadena completa</param>
-        /// <param name="wordToReplace">Cadena a reemplazar</param>
-        /// <param name="newWord">Nueva cadena que añadir</param>
-        /// <returns></returns>
-        private static string ReplaceWordInString(string userString, string wordToReplace, string newWord)
-        {
-            return userString.Replace(wordToReplace, newWord);
-        }
 
-        /// <summary>
-        /// Método encargado de obtener la nueva palabra por la que se quiere sustituir.
-        /// </summary>
-        /// <returns>La nueva palabra que va a sustituir a la anterior.</returns>
-        private static string GetNewWord()
-        {
-            Console.Write("\n¿Por qué palabra quiere sustituirla?: ");
-            string newWord = Console.ReadLine();
-            return newWord;
-        }
+        // Métodos que utiliza el método ReplaceWordOption.
 
         /// <summary>
         /// Método encargado de obtener la cadena que se desea sustituir.
@@ -272,70 +193,155 @@
         }
 
         /// <summary>
-        /// Método encargado de mostrar la despedida al usuario cuando finaliza el programa.
+        /// Método encargado de obtener la nueva palabra por la que se quiere sustituir.
         /// </summary>
-        private static void ExitOption()
+        /// <returns>La nueva palabra que va a sustituir a la anterior.</returns>
+        private static string GetNewWord()
         {
-            Console.Clear();
-            Console.WriteLine("Gracias por usar este software. ¡Hasta la próxima!");
-            Console.ReadKey();
+            Console.Write("\n¿Por qué palabra quiere sustituirla?: ");
+            string newWord = Console.ReadLine();
+            return newWord;
         }
 
-        /// <summary>
-        /// Método encargado de pintar por consola la cadena introducida por el usuario y las opciones que puede realizar.
-        /// </summary>
-        /// <param name="userString"></param>
-        private static void PaintOptionsMenu(string userString)
-        {
-            ShowString();
-
-            Console.WriteLine("\nOpciones:");
-            Console.WriteLine("\t1.- Sustituir palabra.");
-            Console.WriteLine("\t2.- Buscar texto.");
-            Console.WriteLine("\t3.- Buscar texto de inicio.");
-            Console.WriteLine("\t4.- Conversión de dígito.");
-            Console.WriteLine("\t5.- Salir.");
-        }
 
         /// <summary>
-        /// Método encargado de mostrar la cadena que ha introducido el usuario.
+        /// Método que realiza la lógica de sustitución de una palabra por otra dentro de la cadena.
         /// </summary>
-        private static void ShowString()
-        {
-            Console.Clear();
-            Console.WriteLine("La cadena es la siguiente: ");
-            Console.WriteLine(userString);
-        }
-
-        /// <summary>
-        /// Método encargado de pedir al usuario la cadena que se desea utilizar durante el programa.
-        /// </summary>
+        /// <param name="userString">Cadena completa</param>
+        /// <param name="wordToReplace">Cadena a reemplazar</param>
+        /// <param name="newWord">Nueva cadena que añadir</param>
         /// <returns></returns>
-        private static string GetUserString()
+        private static string ReplaceWordInString(string userString, string wordToReplace, string newWord)
         {
-            bool isCorrectString = false;
-            string userString = "";
+            return userString.Replace(wordToReplace, newWord);
+        }
 
-            while (!isCorrectString)
+
+        /// <summary>
+        /// Método encargado de buscar si un texto introducido por el usuario se encuentra dentro de la cadena.
+        /// </summary>
+        private static void SearchTextOption()
+        {
+
+            bool isNotNull = false;
+
+            while (!isNotNull)
             {
-                Console.WriteLine("Introduzca una cadena de, al menos, 40 caracteres: ");
-                userString = Console.ReadLine();
+                Console.Clear();
+                ShowString();
 
+                Console.WriteLine("\nHa elegido la opción Buscar texto: ");
 
-                if (userString.Length >= 40)
+                ShowString();
+
+                Console.Write("\n¿Qué texto desea buscar?: ");
+                string textToSearch = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(textToSearch))
                 {
-                    Console.WriteLine("Cadena válida.");
-
-                    isCorrectString = true;
+                    if (userString.Contains(textToSearch))
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"\nLa cadena \'{textToSearch}\' se encuentra dentro del texto.");
+                        Console.ReadKey();
+                        isNotNull = true;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("La cadena introducida no se encuentra en el texto");
+                        Console.ReadKey();
+                    }
                 }
                 else
                 {
-                    Console.WriteLine($"\nLa cadena introducida posee únicamente {userString.Length} caracteres." +
-                        $" Por favor, introduzca una cadena con, al menos, 40 caracteres.");
+                    Console.Clear();
+                    Console.WriteLine("No puede estar vacía. Introduzca una cadena correcta.");
+                    Console.ReadKey();
                 }
             }
+        }
 
-            return userString;
+        /// <summary>
+        /// Método encargado de comprobar si una cadena empieza por una cadena introducida por el usuario.
+        /// </summary>
+        private static void SearchSringStart()
+        {
+            bool isCorrect = false;
+
+            while (!isCorrect)
+            {
+                Console.Clear();
+                ShowString();
+                Console.WriteLine("Ha elegido la opción Buscar texto de inicio:");
+
+                Console.Write("Introduzca el texto deseado: ");
+                string textToSearch = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(textToSearch))
+                {
+                    if (userString.StartsWith(textToSearch))
+                    {
+                        Console.WriteLine($"\nLa cadena comienza por \'{textToSearch}\'.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\nLa cadena no comienza por \'{textToSearch}\'.");
+                    }
+
+                    Console.ReadKey();
+                    isCorrect = true;
+                }
+                else
+                {
+                    Console.WriteLine("\nDebe añadir un texto. Introduzca una cadena correcta.");
+                    Console.ReadKey();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Método encargado de añadir ceros a la izquierda hasta completar una longitud de 12. 
+        /// </summary>
+        private static void ConvertDigitsOption()
+        {
+            int numberUser;
+
+            bool isNumber = false;
+
+            while (!isNumber)
+            {
+                Console.Clear();
+                Console.WriteLine("Ha elegido la opción Conversión de dígito.");
+                Console.Write("Introduzca un dígito: ");
+                string numberUserString = Console.ReadLine();
+
+                // Comprobando si se ha introducido un número.
+                isNumber = int.TryParse(numberUserString, out numberUser);
+
+                if (isNumber)
+                {
+                    if (numberUserString.Length < 12)
+                    {
+                        int differenceNumber = 12 - numberUserString.Length;
+
+                        // Añade el carácter deseado a la izquierda, en este caso, 0. 
+                        string newStringWithZero = numberUserString.PadLeft(differenceNumber, '0');
+
+                        Console.WriteLine("\nLa cadena resultante es: " + newStringWithZero);
+                        Console.ReadKey();
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nLa cadena tiene un tamaño mayor o igual a 12");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No ha introducido dígitos correctamente.");
+                }
+            }
         }
     }
 }
